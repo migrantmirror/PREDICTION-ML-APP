ffb977f0b118f6f33669c3a9e0134fa2ad4cd8af
migrantmirror <onyekachiebosi@gmail.com>
2025-06-04 10:11:17 +0400

    Update tabs.tsx

--- tabs.tsx
+++ tabs.tsx
@@ -1,14 +1,6 @@
-import * as React from "react"
-import { Tabs as RadixTabs } from "@radix-ui/react-tabs"
-
-const Tabs = React.forwardRef((props, ref) => (
-  <RadixTabs {...props} ref={ref} />
-))
-
-Tabs.TabList = RadixTabs.List
-Tabs.TabTrigger = RadixTabs.Trigger
-Tabs.TabContent = RadixTabs.Content
-
-Tabs.displayName = "Tabs"
-
-export { Tabs }
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "@radix-ui/react-tabs"
